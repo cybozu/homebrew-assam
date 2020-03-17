8a38691f312555f9f07541d88a26f50ab383812c
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-17 15:00:54 +0900

    Brew formula update for assam version v1.2.0

--- assam.rb
+++ assam.rb
@@ -2,9 +2,9 @@
 class Assam < Formula
   desc "Get a credential by AssumeRoleWithSAML for AWS CLI and SDK"
   homepage ""
-  url "https://github.com/cybozu/assam/releases/download/v1.1.1/assam_1.1.1_Darwin_x86_64.tar.gz"
-  version "1.1.1"
-  sha256 "1ceef8362d03ba68d8e0b91fbbb441e7021afcdea6d55189e347767e9f08237c"
+  url "https://github.com/cybozu/assam/releases/download/v1.2.0/assam_1.2.0_Darwin_x86_64.tar.gz"
+  version "1.2.0"
+  sha256 "dd037712c8ddd17f20c1a8d9be868918137f4c2ce46fbdbc93585eca1ccedac1"
 
   def install
     bin.install "assam"
